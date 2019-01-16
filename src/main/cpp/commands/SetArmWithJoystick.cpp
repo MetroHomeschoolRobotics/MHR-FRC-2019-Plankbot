faa772a548b320c54a226e5d20487de8e3630021
GMJJ <gmjj.peterson@gmail.com>
2019-01-15 19:12:41 -0600

    Created command to control the arm with joystick axis 5

---
+++ src/main/cpp/commands/SetArmWithJoystick.cpp
@@ -0,0 +1,35 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "commands/SetArmWithJoystick.h"
+
+SetArmWithJoystick::SetArmWithJoystick() {
+  // Use Requires() here to declare subsystem dependencies
+  // eg. Requires(Robot::chassis.get());
+}
+
+// Called just before this Command runs the first time
+void SetArmWithJoystick::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void SetArmWithJoystick::Execute() {
+  joystickValue=OI.manipulatorJoystick.GetRawAxis(5);
+  currentAngle = Manipulator.GetArmAngle();
+  armMotor->Set(joystickValue);
+}
+
+// Make this return true when this Command no longer needs to run execute()
+bool SetArmWithJoystick::IsFinished() { return false; }
+
+// Called once after isFinished returns true
+void SetArmWithJoystick::End() {}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void SetArmWithJoystick::Interrupted() {
+  End();
+}
