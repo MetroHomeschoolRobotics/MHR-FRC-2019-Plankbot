3a64b5cf96bc978aaab9d2cdf8b761024ffeeb40
GMJJ <gmjj.peterson@gmail.com>
2019-01-15 19:24:28 -0600

    created two commands to collect and release cargo

---
+++ src/main/cpp/commands/CollectCargo.cpp
@@ -0,0 +1,37 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "commands/CollectCargo.h"
+
+CollectCargo::CollectCargo() {
+  // Use Requires() here to declare subsystem dependencies
+  // eg. Requires(Robot::chassis.get());
+}
+
+// Called just before this Command runs the first time
+void CollectCargo::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+//When CollectCargo is called, wheels spin to collect 
+void CollectCargo::Execute() {
+  cargoMotor->Set(-1)
+}
+
+// Make this return true when this Command no longer needs to run execute()
+bool CollectCargo::IsFinished() { return true; }
+
+// Called once after isFinished returns true
+//When ended, motor turns off
+void CollectCargo::End() {
+  cargoMotor->Set(0);
+}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void CollectCargo::Interrupted() {
+  End();
+}
